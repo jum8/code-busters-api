69d05d17915265e4df1f4b01a25535dfbd958d3b
jum8 <jumeight@gmail.com>
2024-02-24 20:39:46 -0300

    fix: Added creation datetime field to challenges and categories

--- src/main/java/dev/codebusters/code_busters/domain/Challenge.java
+++ src/main/java/dev/codebusters/code_busters/domain/Challenge.java
@@ -1,23 +1,18 @@
 package dev.codebusters.code_busters.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.OffsetDateTime;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 @Entity
 @Table(name = "Challenges")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public class Challenge {
@@ -27,7 +22,8 @@ public class Challenge {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private OffsetDateTime added;
 
     @Column
