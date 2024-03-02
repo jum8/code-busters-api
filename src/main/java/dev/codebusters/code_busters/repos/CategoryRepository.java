02fd48ceab6baaa7859bc61fd8a4994b601e1986
jum8 <jumeight@gmail.com>
2024-03-02 19:21:28 -0300

    feat: added endpoint to find all exposed categories

--- src/main/java/dev/codebusters/code_busters/repos/CategoryRepository.java
+++ src/main/java/dev/codebusters/code_busters/repos/CategoryRepository.java
@@ -3,6 +3,9 @@ package dev.codebusters.code_busters.repos;
 import dev.codebusters.code_busters.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByExposedTrue();
 }
